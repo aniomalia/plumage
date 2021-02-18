05b84a2acb3aced0fd2e6c16c457aa3b7cbee51e
Raoul Simionas <raulsimionas@gmail.com>
2021-02-18 21:43:29 +0200

    Update button.php

--- parts/button.php
+++ parts/button.php
@@ -1,9 +1,14 @@
 <?php 
 $post_id = get_the_ID();
 $count = plumage_get_count($post_id);
+$is_author = false;
+global $post;
+if ( is_user_logged_in() && (get_current_user_id() == get_field('resource_meta', $post_id)['original_user']) ) {
+    $is_author = true;
+}
 ?>
 <span class="plumage">
-    <button class="plumage-button<?php global $post; echo ( is_user_logged_in() && get_current_user_id() == $post->post_author ) ? ' user-is-author' : ''; ?><?php echo (get_current_user_id() && user_has_voted($post_id, get_current_user_id())) ? ' has-voted' : ''; ?>" data-id="<?php echo $post_id; ?>">
+    <button class="plumage-button<?php echo $is_author ? ' user-is-author' : ''; ?><?php echo (get_current_user_id() && user_has_voted($post_id, get_current_user_id())) ? ' has-voted' : ''; ?>" data-id="<?php echo $post_id; ?>">
         <span class="plumage-button-loading"></span>
         <span class="plumage-button-icon">
             <span class="plumage-button-mark">
